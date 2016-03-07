4ede8c128465a3f8211387265f9203e7a02e1758
Wai Lee Chin Feman <skatenerd@gmail.com>
2016-03-07 00:08:24 -0500

    Render a triangle lol

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,6 +3,8 @@ module Lib
     ) where
 
 import Data.Function
+import Data.Maybe
+import Control.Monad
 
 data GenericPoint t = Point { pX :: t, pY :: t, pZ :: t } deriving (Show)
 type Point = GenericPoint Float
@@ -18,7 +20,11 @@ type Ray = GenericRay Float
 
 type Plane = Ray
 
+type World = [Triangle]
+
 data Segment  = Segment Point Point deriving (Show)
+
+data ProjectionScreen = ProjectionScreen { xResolution :: Int, yResolution :: Int, screenDirection :: Ray, toLeftEdge :: Vector, toTopEdge :: Vector }
 
 normalTraingleFace :: Triangle -> Vector
 normalTraingleFace t@(Triangle a b c) =
@@ -76,6 +82,14 @@ translateP p v =
     pZ = (vZ v) + (pZ p)
   }
 
+translateV :: Vector -> Vector -> Vector
+translateV v1 v2 =
+  Vector {
+    vX = (vX v1) + (vX v2),
+    vY = (vY v1) + (vY v2),
+    vZ = (vZ v1) + (vZ v2)
+  }
+
 rayIntersectPlane :: Ray -> Plane -> Maybe Point
 rayIntersectPlane ray plane =
   if (isInfinite t)
@@ -101,20 +115,62 @@ rayTriangleIntersection r t =
                  else Nothing
   where planeHit = rayIntersectPlane r (triangleToPlane t)
 
+
+scales :: Int -> [Float]
+scales resolution = [index / (r / 2.0) | index <- stuff]
+  where stuff :: [Float]
+        stuff = fmap (* (-1)) [((-0.5) * r) .. ((0.5) * r)]
+        r :: Float
+        r = fromIntegral resolution
+
+rays screen = [rayRow yScale screen | yScale <- scales (yResolution screen)]
+  where rayRow yScale screen = [mkRay xScale yScale screen | xScale <- scales (xResolution screen)]
+        mkRay xScale yScale screen = Ray {
+          rayOrigin = (rayOrigin (screenDirection screen)),
+          normal = mkVector xScale yScale screen
+        }
+        mkVector xScale yScale screen = translateV
+                                          (screenCenter screen)
+                                          offCenter
+          where offCenter = translateV (scaleV (toLeftEdge screen) xScale)
+                                       (scaleV (toTopEdge screen) yScale)
+        screenCenter screen = pointToVec $ translateP (rayOrigin (screenDirection screen)) (normal (screenDirection screen))
+
+render :: World -> Ray -> Char
+render world r = if (any (hitsTriangle r) world)
+                    then '*'
+                    else ' '
+                 where hitsTriangle r t = isJust (rayTriangleIntersection r t)
+
+
 tests :: IO ()
 tests = let unitX = Vector 1 0 0
             unitY = Vector 0 1 0
+            unitZ = Vector 0 0 1
+            origin = Point 0 0 0
             triangle = Triangle (Point 0 0 0) (Point 10 0 0) (Point 10 0 10)
+            farTriangle = Triangle (Point (-5) 10 0) (Point 0 10 5) (Point 5 10 0)
             plane :: Plane
             plane = Ray {rayOrigin = (Point 0 0 10), normal = (Vector 0 0 (-1)) }
             upray = Ray {rayOrigin = (Point 0 0 0), normal = (Vector 0 0 1) }
             upright = Ray {rayOrigin = (Point 0 0 0), normal = (Vector 5 0 1) }
+            screen = ProjectionScreen {
+              xResolution = 20,
+              yResolution = 20,
+              screenDirection = Ray { rayOrigin=origin, normal=unitY},
+              toLeftEdge = scaleV unitX (-1),
+              toTopEdge = unitZ
+            }
+            world = [farTriangle]
+            rendered = map (map (render world)) (rays screen)
         in
-          (print $ crossProduct unitX unitY) >>
-          (print $ normalTraingleFace triangle) >>
-          (print $ triangleToPlane triangle) >>
-          (print $ rayIntersectPlane upray plane) >>
-          (print $ rayIntersectPlane upright plane) >>
-          (print $ pointInTriangle (Point 1 1 0) (Triangle (Point 0 0 0) (Point 10 0 0) (Point 10 90 0))) >>
-          (print $ pointInTriangle (Point 1 (-1) 0) (Triangle (Point 0 0 0) (Point 10 0 0) (Point 10 90 0)))
+          --(print $ crossProduct unitX unitY) >>
+          --(print $ normalTraingleFace triangle) >>
+          --(print $ triangleToPlane triangle) >>
+          --(print $ rayIntersectPlane upray plane) >>
+          --(print $ rayIntersectPlane upright plane) >>
+          --(print $ pointInTriangle (Point 1 1 0) (Triangle (Point 0 0 0) (Point 10 0 0) (Point 10 90 0))) >>
+          --(print $ pointInTriangle (Point 1 (-1) 0) (Triangle (Point 0 0 0) (Point 10 0 0) (Point 10 90 0))) >>
+          --(print $ scales $ yResolution screen)>>
+          (forM_ rendered putStrLn)
 
