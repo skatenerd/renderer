4eb33a1cb6fe513a82dc7e41e702c5ee7f548fbe
Wai Lee Chin Feman <w.l.chin.feman@gmail.com>
2016-03-10 21:56:32 -0500

    lol more tinkering with demo

--- app/Main.hs
+++ app/Main.hs
@@ -17,7 +17,7 @@ main = let screen = ProjectionScreen {
            farTriangle = Triangle (makePoint (-20) 30 0.5) (makePoint (-10) 30 10) (makePoint 0 30 0.5)
            worlds = map (renderWorld screen) $ iterate updateWorld seedWorld
         in
-          forM_ worlds (\w -> clearScreen >> (forM_ w putStrLn) >> threadDelay 200000)
+          forM_ worlds (\w -> clearScreen >> (forM_ w putStrLn) >> threadDelay 50000)
 
 
 
@@ -25,6 +25,6 @@ updateWorld :: World -> World
 updateWorld (World triangles) = World $ fmap go triangles
   where go :: Triangle -> Triangle
         go t = fmap (pushBack . (rotateAroundCorner t)) t
-        pushBack p = translateP p (Vector 0 0.5 0)
+        pushBack p = translateP p (Vector 0 0.1 0)
         rotateAroundCorner (Triangle _ _ c) p = rotateAround p (Ray c (Vector 0 0 1)) (Radians 0.1)
 
